3c2068b7276d2f53ade742258d7fe29a23d9b5d2
window101 <hwajun15@naver.com>
2021-07-18 10:51:34 +0900

    d

---
+++ models/user.ts
@@ -0,0 +1,42 @@
+
+import { DataTypes, Model } from 'sequelize';
+import { dbType } from './index';
+import { sequelize } from './sequelize';
+
+class User extends Model {
+    public readonly id!: number;
+    public nickname!: string;
+    public userId!: string;
+    public pasword!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
+User.init({
+    nickname: {
+        type: DataTypes.STRING(20),
+    },
+    userId : {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+
+    }
+}, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'user',
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+
+});
+
+export const associate = (db) => {
+
+}
+
+export default User;
